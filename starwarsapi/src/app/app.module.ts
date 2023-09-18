9a65dfe03404c0bea0daffc5ca001a4f2d174f19
u71102 <71102@stu.gbchur.ch>
2023-09-18 14:24:12 +0200

    Planeten suchbar und alle anzeigen möglich

--- starwarsapi/src/app/app.module.ts
+++ starwarsapi/src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { PlanetListComponent } from './planet-list/planet-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlanetListComponent,
+    PlanetListComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- starwarsapi/src/app/planet-list/planet-list.component.ts
+++ starwarsapi/src/app/planet-list/planet-list.component.ts
@@ -1,20 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-planet-list',
   templateUrl: './planet-list.component.html',
-  styleUrls: ['./planet-list.component.css']
 })
-export class PlanetListComponent {
+export class PlanetListComponent implements OnInit {
   planets: any[] = [];
-  apiUrl = 'https://swapi.dev/api/planets/';
+  selectedPlanet: any;
+  searchText: string = '';
 
   constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    this.getPlanets();
+  }
+
   getPlanets() {
-    this.http.get<any>(this.apiUrl).subscribe(data => {
+    this.http.get('https://swapi.dev/api/planets/').subscribe((data: any) => {
       this.planets = data.results;
+    });
+  }
+
+  searchPlanet() {
+    const searchTerm = this.searchText.toLowerCase();
+    if (searchTerm.trim() === '') {
+      this.selectedPlanet = null;
+    } else {
+      const closestMatch = this.planets.find((planet) =>
+        planet.name.toLowerCase().includes(searchTerm)
+      );
+
+      if (closestMatch) {
+        this.getPlanetDetails(closestMatch);
+      } else {
+        this.selectedPlanet = null;
+      }
+    }
+  }
+
+  getPlanetDetails(planet: any) {
+    this.http.get(planet.url).subscribe((data: any) => {
+      this.selectedPlanet = data;
     });
   }
 }
